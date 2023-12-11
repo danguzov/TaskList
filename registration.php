d494bd6eee45d04e6e65b08d146ffeb2756f0f83
danguzovm <gramcodepro@gmail.com>
2023-12-11 21:49:53 +0100

    Update registration

--- registration.php
+++ registration.php
@@ -2,33 +2,41 @@
 
     <link href="css/registration.css" rel="stylesheet">
 
-    <img src="assets/img/cover-table.webp" alt="cover" class="bg">
-    <form action="controllers/registrationUser.php" method="POST">
-    <div class="parent">
-        <div class="centered-div">
-            <h3>Sign Up</h3>
-            <div class="form-floating mb-3">
-                <input type="text" name="first_name" class="form-control" id="floatingInput" placeholder="Name" required>
-                <label for="floatingInput">First Name</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="text" name="last_name" class="form-control" id="floatingInput" placeholder="Name" required>
-                <label for="floatingInput">Last Name</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="E-mail" required>
-                <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-                <label for="floatingPassword">Password</label>
-            </div>
-            <div class="form-floating mb-3">
-                <button type="submit" class="btn btn-success">Sign Up</button>
-            </div>
-        </div>
+    <header>
+    <div class="logo">
+        <i class="fa-solid fa-igloo"></i>
     </div>
-    </form>
+    <div class="title">
+        <h1>TaskList App</h1>
+    </div>
+    </header>
 
+    <form action="controllers/registrationUser.php" method="POST">
+        <section id="signup" style="max-width: 400px; margin: 0 auto; border-radius: 10px;">
+            <h4>Sign Up</h4>
+            <form action="controllers/registrationUser.php" method="POST" class="center-form">
+                <div class="form-floating mb-3">
+                    <input type="text" name="first_name" class="form-control" id="floatingFirstName" placeholder="Name" style="width: 100%;" required>
+                    <label for="floatingFirstName">First Name</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" name="last_name" class="form-control" id="floatingLastName" placeholder="Name" style="width: 100%;" required>
+                    <label for="floatingLastName">Last Name</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="email" name="email" class="form-control" id="floatingEmail" placeholder="E-mail" style="width: 100%;" required>
+                    <label for="floatingEmail">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" style="width: 100%;" required>
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <button type="submit" class="btn btn-success" style="width: 100%;">Sign Up</button>
+                </div>
+            </form>
+            <p>Already have an account? <a href="index.php">Log In</a></p>
+        </section>
+    </form>
 
 <?php require_once "views/footer.php"; ?>
