f5c35e40e0385292c2a0c554ff3fa87bd78945cd
danguzovm <gramcodepro@gmail.com>
2024-04-16 21:04:36 +0200

    Add tailwindcss elements

--- index.php
+++ index.php
@@ -2,31 +2,46 @@
 
 <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 
-<?php require_once "views/navbar.php" ?>
-
 <div class="container col-xl-10 col-xxl-8 px-4 py-5">
     <div class="row align-items-center g-lg-5 py-5">
-        <div class="col-lg-7 text-center text-lg-start">
-            <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Task List App</h1>
-            <p class="col-lg-10 fs-4">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</p>
-        </div>
-
-        <div class="col-md-10 mx-auto col-lg-5">
-            <h4>Welcome Back</h4>
-            <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary" action="controllers/loginController.php" method="POST">
-                <div class="form-floating mb-3">
-                    <input type="email" name="email" class="form-control" id="floatingInput" required>
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" name="password" class="form-control" id="floatingPassword" required>
-                    <label for="floatingPassword">Password</label>
+        <div class="col-lg-7">
+            <div class="flex justify-between">
+                <div>
+                    <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Task List App</h1>
+                    <p class="col-lg-10 fs-4">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</p>
                 </div>
 
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Log In</button>
-                <hr class="my-4">
-                <small class="text-body-secondary">Log In and continue with your tasks</small>
-            </form>
+                <div class="w-1/2"> <!-- Postavljamo širinu forme na polovinu kako bi bila sa desne strane -->
+                    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log in to your account</h2>
+                        <form class="mt-10 space-y-6" action="controllers/loginController.php" method="POST">
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div class="mt-2">
+                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        </div>
+                        <div class="mt-2">
+                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+                    </div>
+                        </form>
+                        <p class="mt-10 text-center text-sm text-gray-500">
+                            Not a member?
+                            <a href="views/registration.php" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign up here</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -34,3 +49,25 @@
 <?php  require_once "views/footer.php"?>
 
 
+        <!--
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+        <!--
+          This example requires updating your template:
+
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        -->
--- views/registration.php
+++ views/registration.php
@@ -17,43 +17,54 @@
     }
 ?>
 
-    <link href="../assets/css/registration.css" rel="stylesheet">
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
 
-    <?php require_once "navbar.php" ?>
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+        </div>
 
-    <form action="../controllers/registrationController.php" method="POST">
-        <section id="signup" style="max-width: 400px; margin: 0 auto; border-radius: 10px;">
-
-            <h4>Sign Up</h4>
-
-            <form action="" method="POST" class="center-form">
-                <div class="form-floating mb-3">
-                    <input type="text" name="first_name" class="form-control" id="floatingFirstName" placeholder="First name" style="width: 100%;" required>
-                    <label for="floatingFirstName">First Name</label>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="../controllers/registrationController.php" method="POST">
+                <div>
+                    <label for="text" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                    <div class="mt-2">
+                        <input id="email" name="first_name" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div>
+                    <label for="text" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                    <div class="mt-2">
+                        <input id="email" name="last_name" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
                 </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" name="last_name" class="form-control" id="floatingLastName" placeholder="Last name" style="width: 100%;" required>
-                    <label for="floatingLastName">Last Name</label>
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    </div>
+                    <div class="mt-2">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
                 </div>
 
-                <div class="form-floating mb-3">
-                    <input type="email" name="email" class="form-control" id="floatingEmail" placeholder="E-mail" style="width: 100%;" required>
-                    <label for="floatingEmail">Email address</label>
+                <div>
+                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                 </div>
 
-                <div class="form-floating">
-                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" style="width: 100%;" required>
-                    <label for="floatingPassword">Password</label>
-                </div>
-
-                <div class="form-floating mb-3">
-                    <button type="submit" class="btn btn-success" style="width: 100%;">Sign Up</button>
-                </div>
+                <p class="mt-10 text-center text-sm text-gray-500">
+                    Already have an account?
+                    <a href="../index.php" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Back to Log in</a>
+                </p>
             </form>
-            <p>Already have an account? <a href="../index.php">Back to Log in</a>
-        </section>
-    </form>
+
+        </div>
+    </div>
 
 <?php require_once "footer.php"; ?>
 
