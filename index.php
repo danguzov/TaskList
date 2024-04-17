2c3f4c64a79bdc458c67d3a7712b5cbedd92c94f
danguzovm <gramcodepro@gmail.com>
2024-04-17 09:37:15 +0200

    Add landing page adn error page

--- index.php
+++ index.php
@@ -1,47 +1,62 @@
 <?php include "include/header.php"  ?>
 
-<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+<div class="hidden lg:flex lg:flex-1 lg:justify-end">
+    <a href="views/login.php" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+</div>
 
-<div class="container col-xl-10 col-xxl-8 px-4 py-5">
-    <div class="row align-items-center g-lg-5 py-5">
-        <div class="col-lg-7">
-            <div class="flex justify-between">
-                <div>
-                    <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Task List App</h1>
-                    <p class="col-lg-10 fs-4">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</p>
+<div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:text-center">
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Task List App</p>
+            <p class="mt-6 text-lg leading-8 text-gray-600">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7 text-gray-900">
+                        <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                            <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>
+                        </div>
+                        Push to deploy
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-600">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</dd>
                 </div>
-
-                <div class="w-1/2"> <!-- Postavljamo širinu forme na polovinu kako bi bila sa desne strane -->
-                    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log in to your account</h2>
-                        <form class="mt-10 space-y-6" action="controllers/loginController.php" method="POST">
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7 text-gray-900">
+                        <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                            <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
                         </div>
-                    </div>
-
-                    <div>
-                        <div class="flex items-center justify-between">
-                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        </div>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
-                    </div>
-                        </form>
-                        <p class="mt-10 text-center text-sm text-gray-500">
-                            Not a member?
-                            <a href="views/registration.php" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign up here</a>
-                        </p>
-                    </div>
+                        SSL certificates
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-600">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</dd>
                 </div>
-            </div>
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7 text-gray-900">
+                        <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                            <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg>
+                        </div>
+                        Simple queues
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-600">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</dd>
+                </div>
+                <div class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7 text-gray-900">
+                        <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                            <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
+                            </svg>
+                        </div>
+                        Advanced security
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-600">"Experience seamless task management with our intuitive <em>Task List App</em>. Easily input, edit, delete, and check off tasks to stay organized and productive. Streamline your workflow and never miss a deadline again."</dd>
+                </div>
+            </dl>
         </div>
     </div>
 </div>
